960adf9aaf1fa8b3fa6deb7771b53fcb45c78e9e
dBm <15308418+dBarmpxkos@users.noreply.github.com>
2019-02-09 17:33:12 +0200

    Add varargin to extract_features

--- MATLAB/dot_counter_prototype/extract_features.m
+++ MATLAB/dot_counter_prototype/extract_features.m
@@ -2,27 +2,40 @@
 %{
 	This script reads rgb images from the patterns we are developing,
 	converts them to grayscale, to BW and extracts features for classification.
-	Adding features BW pixeldiff, circle count
+
+	Features: BW pixeldiff, circle count, skew and skew deriv. min/max
 %}
-function [diffs, numberOfCircles, skEwMAX, diffSkEwMAX, skEwMIN, diffSkEwMIN] = extract_features(x)
-%% data input -------------------------------------------------------------
-rgbImage 	= imread(x);
-grayImage 	= rgb2gray(rgbImage);  
-BWImage 	= im2bw(grayImage);
+function [featureMatrix] = extract_features(varargin)
 
-% Features ----------------------------------------------------------------
+for imageCount = 1:nargin
+	%% data input -------------------------------------------------------------
+	rgbImage 	= imread(varargin{imageCount});
+	grayImage 	= rgb2gray(rgbImage);  
+	BWImage 	= im2bw(grayImage);
 
-%% DIFFS
-histogramBW = imhist(BWImage);
-diffs = histogramBW(1) - histogramBW(2)
+	% Features ----------------------------------------------------------------
 
-%% find circles
-binaryImage = rgbImage(:,:,1) < 200;
-numberOfCircles = length(regionprops(binaryImage, 'Centroid', 'Area'))
+	%% DIFFS
+	histogramBW = imhist(BWImage);
+	diffs = histogramBW(1) - histogramBW(2)
 
-%% skew / diff_skew / Max-Min
-skEw = skewness(BWImage);
-skEwMAX = max(skEw)
-diffSkEwMAX = max(diff(skEw))
-skEwMIN = min(skEw)
-diffSkEwMIN = min(diff(skEw))
+	%% find circles
+	binaryImage = rgbImage(:,:,1) < 200;
+	numberOfCircles = length(regionprops(binaryImage, 'Centroid', 'Area'))
+
+	%% skew / diff_skew / Max-Min
+	skEw = skewness(BWImage);
+	skEwMAX = max(skEw)
+	diffSkEwMAX = max(diff(skEw))
+	skEwMIN = min(skEw)
+	diffSkEwMIN = min(diff(skEw))
+
+	%% export in matrix
+	featureMatrix(imageCount, 1) = imageCount;
+	featureMatrix(imageCount, 2) = diffs;
+	featureMatrix(imageCount, 3) = numberOfCircles;
+	featureMatrix(imageCount, 4) = skEwMAX;
+	featureMatrix(imageCount, 5) = diffSkEwMAX;
+	featureMatrix(imageCount, 6) = skEwMIN;
+	featureMatrix(imageCount, 7) = diffSkEwMIN;
+end
