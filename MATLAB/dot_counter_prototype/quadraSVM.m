fbde2290615f2012ada8bdae0710d5f4fc4cd731
dBm <15308418+dBarmpxkos@users.noreply.github.com>
2019-02-09 17:55:42 +0200

    Add classifiers with 100% acccuracy

---
+++ MATLAB/dot_counter_prototype/quadraSVM.m
@@ -0,0 +1,73 @@
+function [trainedClassifier, validationAccuracy] = trainClassifier(trainingData)
+inputTable = array2table(trainingData, 'VariableNames', {'column_1', 'column_2', 'column_3', 'column_4', 'column_5', 'column_6', 'column_7'});
+
+predictorNames = {'column_2', 'column_3', 'column_4', 'column_5', 'column_6', 'column_7'};
+predictors = inputTable(:, predictorNames);
+response = inputTable.column_1;
+isCategoricalPredictor = [false, false, false, false, false, false];
+
+% Apply a PCA to the predictor matrix.
+% Run PCA on numeric predictors only. Categorical predictors are passed through PCA untouched.
+isCategoricalPredictorBeforePCA = isCategoricalPredictor;
+numericPredictors = predictors(:, ~isCategoricalPredictor);
+numericPredictors = table2array(varfun(@double, numericPredictors));
+% 'inf' values have to be treated as missing data for PCA.
+numericPredictors(isinf(numericPredictors)) = NaN;
+[pcaCoefficients, pcaScores, ~, ~, explained, pcaCenters] = pca(...
+    numericPredictors);
+% Keep enough components to explain the desired amount of variance.
+explainedVarianceToKeepAsFraction = 95/100;
+numComponentsToKeep = find(cumsum(explained)/sum(explained) >= explainedVarianceToKeepAsFraction, 1);
+pcaCoefficients = pcaCoefficients(:,1:numComponentsToKeep);
+predictors = [array2table(pcaScores(:,1:numComponentsToKeep)), predictors(:, isCategoricalPredictor)];
+isCategoricalPredictor = [false(1,numComponentsToKeep), true(1,sum(isCategoricalPredictor))];
+
+% Train a classifier
+% This code specifies all the classifier options and trains the classifier.
+template = templateSVM(...
+    'KernelFunction', 'polynomial', ...
+    'PolynomialOrder', 2, ...
+    'KernelScale', 'auto', ...
+    'BoxConstraint', 1, ...
+    'Standardize', true);
+classificationSVM = fitcecoc(...
+    predictors, ...
+    response, ...
+    'Learners', template, ...
+    'Coding', 'onevsone', ...
+    'ClassNames', [1; 2; 3; 4]);
+
+% Create the result struct with predict function
+predictorExtractionFcn = @(x) array2table(x, 'VariableNames', predictorNames);
+pcaTransformationFcn = @(x) [ array2table((table2array(varfun(@double, x(:, ~isCategoricalPredictorBeforePCA))) - pcaCenters) * pcaCoefficients), x(:,isCategoricalPredictorBeforePCA) ];
+svmPredictFcn = @(x) predict(classificationSVM, x);
+trainedClassifier.predictFcn = @(x) svmPredictFcn(pcaTransformationFcn(predictorExtractionFcn(x)));
+
+% Add additional fields to the result struct
+trainedClassifier.PCACenters = pcaCenters;
+trainedClassifier.PCACoefficients = pcaCoefficients;
+trainedClassifier.ClassificationSVM = classificationSVM;
+trainedClassifier.About = 'This struct is a trained model exported from Classification Learner R2018a.';
+trainedClassifier.HowToPredict = sprintf('To make predictions on a new predictor column matrix, X, use: \n  yfit = c.predictFcn(X) \nreplacing ''c'' with the name of the variable that is this struct, e.g. ''trainedModel''. \n \nX must contain exactly 6 columns because this model was trained using 6 predictors. \nX must contain only predictor columns in exactly the same order and format as your training \ndata. Do not include the response column or any columns you did not import into the app. \n \nFor more information, see <a href="matlab:helpview(fullfile(docroot, ''stats'', ''stats.map''), ''appclassification_exportmodeltoworkspace'')">How to predict using an exported model</a>.');
+
+% Extract predictors and response
+% This code processes the data into the right shape for training the
+% model.
+% Convert input to table
+inputTable = array2table(trainingData, 'VariableNames', {'column_1', 'column_2', 'column_3', 'column_4', 'column_5', 'column_6', 'column_7'});
+
+predictorNames = {'column_2', 'column_3', 'column_4', 'column_5', 'column_6', 'column_7'};
+predictors = inputTable(:, predictorNames);
+response = inputTable.column_1;
+isCategoricalPredictor = [false, false, false, false, false, false];
+
+validationPredictFcn = @(x) svmPredictFcn(pcaTransformationFcn(x));
+
+% Compute resubstitution predictions
+[validationPredictions, validationScores] = validationPredictFcn(predictors);
+
+% Compute validation accuracy
+correctPredictions = (validationPredictions == response);
+isMissing = isnan(response);
+correctPredictions = correctPredictions(~isMissing);
+validationAccuracy = sum(correctPredictions)/length(correctPredictions);
