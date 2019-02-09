a36f45d1e15783e5af736350a68bc89f2e7e2962
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2019-02-09 13:27:26 +0200

    add tailwind

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  render() {
-    return <div className="App">It's Alive!</div>;
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1 className="text-blue">It's Alive!</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
