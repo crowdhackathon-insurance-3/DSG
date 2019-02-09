da5d179b00d025cab18e8809176698d73201f87b
dBm <15308418+dBarmpxkos@users.noreply.github.com>
2019-02-09 13:42:08 +0200

    Add gray2bw -> hist

--- MATLAB/dot_counter_prototype/bw_counter.m
+++ MATLAB/dot_counter_prototype/bw_counter.m
@@ -1,2 +1,12 @@
-%% read image, convert to grayscale, extract histogram 
-plot(imhist(rgb2gray(imread('test.png'))))
+%% read image, convert to grayscale, convert to bw, extract histogram 
+% imhist( im2bw(rgb2gray(imread('0.png')), 0.5));
+
+image_0 = rgb2gray(imread('0.png'));  
+image_1 = rgb2gray(imread('1.png'));
+image_2 = rgb2gray(imread('2.png'));
+image_3 = rgb2gray(imread('3.png'));
+
+hist0 = imhist( im2bw(rgb2gray(imread('0.png')), 0.5));
+hist1 = imhist( im2bw(rgb2gray(imread('1.png')), 0.5));
+hist2 = imhist( im2bw(rgb2gray(imread('2.png')), 0.5));
+hist3 = imhist( im2bw(rgb2gray(imread('3.png')), 0.5));
